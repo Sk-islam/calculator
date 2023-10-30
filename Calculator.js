d6c52c886e03b228bd8b973537a53800031b5d44
Sk-islam <138716556+Sk-islam@users.noreply.github.com>
2023-10-31 01:03:36 +0530

    Add files via upload

---
+++ Calculator.js
@@ -0,0 +1,22 @@
+let output=document.getElementById('output-screen');
+
+function display(num){
+    output.value +=num;
+}
+
+function Calculate(){
+    try{
+        output.value = eval(output.value);
+    }
+    catch(err){
+        alert("Invalid")
+    }
+}
+
+function Clear(){
+    output.value = "";
+}
+
+function del(){
+    output.value = output.value.slice(0,-1);
+}
